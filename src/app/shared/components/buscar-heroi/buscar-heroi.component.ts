b57b58ee0f9b192a341f5d941c5c5a27ec695e4d
CarloSilva96 <carlosilva.dev@gmail.com>
2022-02-08 21:37:28 -0300

    Criado subject em buscar-heroi-compent responsável de receber a lista dos herois buscados e vinculado autoComplete ao input

--- src/app/shared/components/buscar-heroi/buscar-heroi.component.ts
+++ src/app/shared/components/buscar-heroi/buscar-heroi.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Observable, Subject, switchMap} from "rxjs";
+import {Heroi} from "../../../core/models/heroi.model";
+import {HeroiService} from "../../../core/services/heroi.service";
 
 @Component({
   selector: 'app-buscar-heroi',
@@ -6,10 +9,27 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./buscar-heroi.component.scss']
 })
 export class BuscarHeroiComponent implements OnInit {
+  /** Variavel do tipo observable **/
+  herois$!: Observable<Heroi[]>;
   @Input() label = '';
-  constructor() { }
+
+  private buscarTermos = new Subject<string>();
+
+  constructor(
+    private heroiService: HeroiService
+  ) { }
 
   ngOnInit(): void {
+    this.herois$ = this.buscarTermos.pipe(
+      /** Pegando o valor atualizado de termo e passando para busca de heroi **/
+      /** o switchMap usa intervalos para trabalhar com isso **/
+      switchMap(termo => this.heroiService.buscarHeroi(termo))
+    );
+  }
+
+  buscar(termo: string): void {
+    /** Recebe o valor na variavel e vai atualizado o subject buscarTermos **/
+    this.buscarTermos.next(termo);
   }
 
 }
